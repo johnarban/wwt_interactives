a7f7f01eee204b1fba5cc3fc1e3d498637d03afb
John Arban Lewis <johnarban@users.noreply.github.com>
2023-01-30 14:05:24 -0500

    add all images

--- aladin_light/aladin.js
+++ aladin_light/aladin.js
@@ -17,8 +17,7 @@ aladin.addCatalog(catalog);
 aladin.gotoRaDec("237.742", "32.047")
 aladin.setFoV(6)
 var url1 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/stsci.jpg'
-var url3 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/img_tagged.jpg'
-var url4 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/692_2022E3_02_01_23_tagged.jpg'
+var url2 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/comet_inverted_tagged.jpg'
 
-aladin.displayJPG(url4,);
+aladin.displayJPG(url2,);
 // 
