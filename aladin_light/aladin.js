1d2c82b05e67e2aa24d73706437899d2578f03ef
John Arban Lewis <johnarban@users.noreply.github.com>
2023-11-01 14:00:35 -0400

    add tagged

--- aladin_light/aladin.js
+++ aladin_light/aladin.js
@@ -14,10 +14,11 @@ draper_options = {
 catalog = A.catalogFromVizieR(draper, '19:50 +16:50', 5, draper_options)
 // aladin.addCatalog(catalog);
 // add gaia catalog
-aladin.gotoRaDec("287.87924128945207", "16.860364447073678")
+aladin.gotoRaDec("266.5", "-28.7")
 aladin.setFoV(6)
-var url1 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/stsci.jpg'
-var url2 = 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2023/03/wolf-rayet_124_nircam_and_miri_composite_image/24749635-1-eng-GB/Wolf-Rayet_124_NIRCam_and_MIRI_composite_image.jpg'
+// var url1 = 'https://raw.githubusercontent.com/johnarban/wwt_interactives/main/images/stsci.jpg'
+// var url2 = 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2023/03/wolf-rayet_124_nircam_and_miri_composite_image/24749635-1-eng-GB/Wolf-Rayet_124_NIRCam_and_MIRI_composite_image.jpg'
+var url2 = 'https://johnarban.github.io/wwt_interactives/images/adam/BrickJWST_merged_longwave_narrowband_lighter_avm_tagged.png'
 
 aladin.displayJPG(url2, {}, (ra, dec,fov) => {
     console.log('displayJPG callback', ra, dec, fov)
